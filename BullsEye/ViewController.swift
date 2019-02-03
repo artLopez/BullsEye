43b3a7492167747a71df8552374baab73e44f102
Arturo Lopez <alopez@delegata.com>
2019-02-02 20:55:24 -0800

    Initial Commit

---
+++ BullsEye/ViewController.swift
@@ -0,0 +1,73 @@
+//
+//  ViewController.swift
+//  BullsEye
+//
+//  Created by Arturo Lopez on 2/2/19.
+//  Copyright © 2019 Arturo Lopez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var score: Int = 0
+    var round: Int = 0
+    var targetValue: Int = 0
+    var currentSliderValue : Int = 0
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var targetLabel: UILabel!
+    @IBOutlet weak var roundLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        currentSliderValue = Int(slider.value.rounded())
+        startNewRound()
+        
+    }
+    
+    @IBAction func showAlert(){
+        let message = "The value of the slider is now: \(currentSliderValue) " +
+                      "\n The target value is : \(targetValue)"
+        
+        let alert = UIAlertController(title: "Hello World", message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil )
+        startNewRound()
+    }
+    
+    @IBAction func sliderMoved(_ slider: UISlider) {
+        let roundedValue = slider.value.rounded()
+        currentSliderValue = Int(roundedValue)
+    }
+    
+    @IBAction func resetGame(){
+        score = 0;
+        round = 0;
+        
+        startNewRound()
+    }
+    
+    func startNewRound() {
+        targetValue = Int.random(in: 1...100)
+        currentSliderValue = 0
+        slider.value = Float(currentSliderValue)
+        
+        round += 1
+        updateLabels()
+    }
+    
+    func updateLabels() {
+        targetLabel.text = String(targetValue)
+        roundLabel.text = String(round)
+        
+    }
+
+
+}
+
